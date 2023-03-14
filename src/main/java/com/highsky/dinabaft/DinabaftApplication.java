c3e524b9e7a0aea89c8f5fdc1b3bf50d483f77bf
ghasemizadian <arman.dev.one@gmail.com>
2023-03-14 08:37:02 +0100

    raw project downloaded from spring.io

---
+++ src/main/java/com/highsky/dinabaft/DinabaftApplication.java
@@ -0,0 +1,13 @@
+package com.highsky.dinabaft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinabaftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinabaftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/highsky/dinabaft/DinabaftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.highsky.dinabaft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinabaftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
